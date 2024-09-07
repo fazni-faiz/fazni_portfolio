0d22b77e30f575532afddbaea275f0ad30cdbc2a
fazni-faiz <faznifaiz6@gmail.com>
2024-09-07 11:44:08 +0530

    update 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import fazniImage from '../img/fazni.jpeg';
 
 const Header = () => {
   return (
-    <header className="bg-black text-white p-6 shadow-lg relative">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-black text-white p-4 md:p-6 shadow-lg relative">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         {/* Left Side: Name and Navigation */}
-        <div className="flex items-center space-x-4">
-          <h1 className="text-4xl font-bold"> FAZNI FAIZ</h1>
+        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6">
+          <h1 className="text-3xl md:text-4xl font-bold">FAZNI FAIZ</h1>
           <nav>
-            <ul className="flex space-x-6 text-lg">
+            <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 text-lg">
               <li><a href="#profile" className="hover:text-gray-400">Profile</a></li>
               <li><a href="#education" className="hover:text-gray-400">Education</a></li>
               <li><a href="#experience" className="hover:text-gray-400">Experience</a></li>
@@ -20,8 +20,8 @@ const Header = () => {
           </nav>
         </div>
 
-        
-        <div className="w-16 h-16 md:w-24 md:h-24 rounded-full overflow-hidden border-4 border-white">
+        {/* Right Side: Image */}
+        <div className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 rounded-full overflow-hidden border-4 border-white mt-4 md:mt-0">
           <img src={fazniImage} alt="Mohamed Fais Mohamed Fasni" className="w-full h-full object-cover"/>
         </div>
       </div>
